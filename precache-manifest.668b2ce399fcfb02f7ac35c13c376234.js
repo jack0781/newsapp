dd8c2b951c0d6d2bea258c1ea786a485d36ad292
jack0781 <jaykatakiya8946@gmail.com>
2021-03-04 12:30:39 +0530

    Updates

---
+++ precache-manifest.668b2ce399fcfb02f7ac35c13c376234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3f8f2ddde5093d8f1f9186ff6b1677f",
+    "url": "/newsapp/index.html"
+  },
+  {
+    "revision": "8e9d0f5f1320b09e9430",
+    "url": "/newsapp/static/css/main.66e661ce.chunk.css"
+  },
+  {
+    "revision": "4f7c2056fe76cc07a84d",
+    "url": "/newsapp/static/js/2.0508f805.chunk.js"
+  },
+  {
+    "revision": "8e9d0f5f1320b09e9430",
+    "url": "/newsapp/static/js/main.9095977d.chunk.js"
+  },
+  {
+    "revision": "e38b28ac8aa4d473c07b",
+    "url": "/newsapp/static/js/runtime~main.55a81b73.js"
+  },
+  {
+    "revision": "013fb4a1289cba588aa7a2c343028905",
+    "url": "/newsapp/static/media/news-default-image.013fb4a1.jpg"
+  }
+]);
